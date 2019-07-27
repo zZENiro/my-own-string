f6c80f9077b3f803bb6a983e35bba1a7464597f6
Никита Ведерников <soloistcsgo@yandex.ru>
2019-07-28 02:31:05 +0500

    inserted .cpp files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    std::map<int, string> abc;
+    abc.insert(std::pair<int, string>(1, "Hello"));
+
+    return 0;
+}
